b505d1ff72333682d686a2bf14f583dc8ddffe18
poojaJ214 <technicalupgradationhub@gmail.com>
2025-07-11 18:11:45 +0530

    now added routes in app.jsx file

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -7,15 +7,18 @@ import Register from './pages/Register/Register'
 import Home from './pages/Home/Home'
 import Orders from './pages/Orders/Orders'
 import Cart from './pages/Cart/Cart'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Login/>
-      <Register/>
-      <Home/>
-      <Orders/>
-      <Cart/>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/orders' element={<Orders/>}/>
+      <Route path='/home' element={<Home/>}/>
+    </Routes>
     </div>
   )
 }
