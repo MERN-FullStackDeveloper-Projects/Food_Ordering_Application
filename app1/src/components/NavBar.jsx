1742afabd3f6354340cb384006cf775d012bc236
poojaJ214 <technicalupgradationhub@gmail.com>
2025-07-11 22:36:00 +0530

    logout navigate functionality done

--- app1/src/components/NavBar.jsx
+++ app1/src/components/NavBar.jsx
@@ -1,42 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function NavBar() {
+  //get the navigate function reference
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    //remove all cached data (name, token)
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("email");
+
+    //go to the login screen
+    navigate("/");
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-primary " data-bs-theme="dark">
-              <div className="container-fluid">
-                <Link className="navbar-brand" to='/home'>Food App</Link>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarNavAltMarkup"
-                  aria-controls="navbarNavAltMarkup"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div className="navbar-nav">
-                    <Link className="nav-link active" aria-current="page" to='/home'>
-                      Home
-                    </Link>
-                    <Link className="nav-link" to='/cart'>
-                      Cart
-                    </Link>
-                    <Link className="nav-link" to='/orders' >
-                      Orders
-                    </Link>
-                    <Link className="nav-link disabled" aria-disabled="true">
-                      Logout
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </nav>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/home">
+            Food App
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" aria-current="page" to="/home">
+                Home
+              </Link>
+              <Link className="nav-link" to="/cart">
+                Cart
+              </Link>
+              <Link className="nav-link" to="/orders">
+                Orders
+              </Link>
+              <button
+                onClick={onLogout}
+                // className="nav-link disabled"
+                className="nav-link"
+                aria-disabled='true'
+              
+              >
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
