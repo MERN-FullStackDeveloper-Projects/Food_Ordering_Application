8909c87dcc915f27ceb67423ac57efeb7413e285
poojaJ214 <technicalupgradationhub@gmail.com>
2025-07-11 22:07:03 +0530

    adding here in home.jsx bootstrap navbar code and import link its not a good wring a code so my redundacy increase  so i remove this

--- app1/src/pages/Cart/Cart.jsx
+++ app1/src/pages/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import './Cart.css'
 function Cart() {
   return (
     <div>
+     
       <h2 className="page-header">Cart Page</h2>
     </div>
   )
--- app1/src/pages/Home/Home.jsx
+++ app1/src/pages/Home/Home.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import './Home.css'
-
+import React from "react";
+import "./Home.css";
+import { Link} from "react-router-dom";
 function Home() {
   return (
     <div>
-      <h2 className='page-header'>Home Pge</h2>
+      <nav className="navbar navbar-expand-lg bg-primary " data-bs-theme="dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to='/home'>Food App</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" aria-current="page" to='/home'>
+                Home
+              </Link>
+              <Link className="nav-link" to='/cart'>
+                Cart
+              </Link>
+              <Link className="nav-link" to='/orders' >
+                Orders
+              </Link>
+              <Link className="nav-link disabled" aria-disabled="true">
+                Logout
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <h2 className="page-header">Home Pge</h2>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
