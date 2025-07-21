b8b7e550aa42a58040d17d1ac6c73c51c0fc2e36
poojaJ214 <technicalupgradationhub@gmail.com>
2025-07-21 12:11:24 +0530

    add to cart functinalitry done using useEffect() conditional rendering

--- app1/src/pages/Products/Products.jsx
+++ app1/src/pages/Products/Products.jsx
@@ -1,71 +1,77 @@
-
 import React, { useEffect, useState } from "react";
 import { loadCatalog } from "../../services/catalog";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 import { config } from "../../services/config";
-import './Products.css'
+import "./Products.css";
 import { useDispatch } from "react-redux";
 import { addToCartAction } from "../../slices/cartSlice";
 
 function Products() {
-const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
 
-//get the reference of dispatch function
-const dispatch = useDispatch()
+  //get the reference of dispatch function
+  const dispatch = useDispatch();
 
-    const getCatalog = async () => {
-  const result = await loadCatalog()
-  if (result['status'] == 'success') {
-    setItems(result['data'])
-    console.log(result)
-  }else{
-    toast.error(result['error'])
-  }
-  
-   console.log(result)
+  const getCatalog = async () => {
+    const result = await loadCatalog();
+    if (result["status"] == "success") {
+      setItems(result["data"]);
+      console.log(result);
+    } else {
+      toast.error(result["error"]);
+    }
 
-}
-useEffect(() =>{
-  console.log(`product components loaded`)
-  //load the catalog
-  getCatalog()
+    console.log(result);
+  };
+  useEffect(() => {
+    console.log(`product components loaded`);
+    //load the catalog
+    getCatalog();
 
-   return () =>{
-     console.log(`product components unloaded`)
-   }
-},[])
+    return () => {
+      console.log(`product components unloaded`);
+    };
+  }, []);
 
-const onAddToCart = (item) => {
-  //invote add to cart action
-dispatch(addToCartAction(item))
-}
+  const onAddToCart = (item) => {
+    //invote add to cart action
+    dispatch(addToCartAction(item));
+  };
 
   return (
-    <div>    
-      <h2 className='page-header'> Products</h2>
+    <div>
+      <h2 className="page-header"> Products</h2>
       {/* <button onClick={getCatalog}>load Catalog</button> */}
-     <div className="row mt-3">
-      {items.map(item => {
-        const imageUrl = `${config.serverBaseUrlCatalog}/${item['image']}`
-        console.log('Image URL:', imageUrl)
-        return  <div className="col-3">
-        <div className="card" style={{position:'relative'}}>
-          <img
-          style={{height: 200}}
-          src={imageUrl} className="card-img-top" alt="..."></img>
-      <button onClick={() => onAddToCart(item)} className="btn btn-success cart-button">Cart</button>
-          <div className="card-body">
-    <h5 className="card-title">{item['title']}</h5>
-    <p className="card-text">{item['description']}</p>
-    
-  </div>
-        </div>
+      <div className="row mt-3">
+        {items.map((item) => {
+          const imageUrl = `${config.serverBaseUrlCatalog}/${item["image"]}`;
+          console.log("Image URL:", imageUrl);
+          return (
+            <div className="col-3" key={item.id}>
+              <div className="card" style={{ position: "relative" }}>
+                <img
+                  style={{ height: 200 }}
+                  src={imageUrl}
+                  className="card-img-top"
+                  alt="..."
+                ></img>
+                <button
+                  onClick={() => onAddToCart(item)}
+                  className="btn btn-success cart-button"
+                >
+                  Cart
+                </button>
+                <div className="card-body">
+                  <h5 className="card-title">{item["title"]}</h5>
+                  <p className="card-text">{item["description"]}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      })}
-
-     </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
--- app1/src/slices/cartSlice.js
+++ app1/src/slices/cartSlice.js
@@ -7,20 +7,37 @@ const cartSlice = createSlice({
     reducers:{
         addToCartAction: (state, {payload}) => {
             //check if the product already present
-            if(state.items.findIndex(item => item.id == payload.id) != -1 ){
-                //teh item does not exist in the cart
+            if(state.items.findIndex(item => item.id == payload.id) == -1 ){
+                //the item does not exist in the cart
                 //add the item push  to the items list
-                state.items.push(payload)
+                state.items.push({...payload, quantity:1})
             }
 console.log(state)
 console.log(payload)
         },
         removeFromCartAction: (state,{payload}) => {
+const {id} = payload
+// console.log(`id: ${id},quantity: ${quantity}`)
+//find the product which matcges with the id
+const index = state.items.findIndex((item) => item.id == id)
+console.log(`index = ${index}`)
+if (index > -1) {
+    state.items.splice(index, 1)
+}
 
-            console.log(state)
-console.log(payload)
+//             console.log(state)
+// console.log(payload)
         },
+        updateQuantityAction: (state, {payload})=> {
+//  console.log(state)
+// console.log(payload)
+const {id, quantity} = payload
+// console.log(`id: ${id},quantity: ${quantity}`)
+//find the product which matcges with the id
+const index = state.items.findIndex((item) => item.id == id)
+state.items[index].quantity += quantity
+        }
     },
 })
-export const {addToCartAction, removeFromCartAction} = cartSlice.actions
+export const {addToCartAction, removeFromCartAction, updateQuantityAction} = cartSlice.actions
 export default cartSlice.reducer
